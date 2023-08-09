6dc24d26f09c40860ffea11828084943473fe682
Sir-Sylvester <78985093+Sir-Sylvester@users.noreply.github.com>
2023-08-09 19:39:24 +0000

    Day1 finished

---
+++ Day1/datatypes.js
@@ -0,0 +1,46 @@
+// Number 5
+
+let Name = "Sylvester"
+console.log(typeof Name)
+let text = null
+console.log(typeof text)
+let number =22
+console.log(typeof number)
+let statement = true
+console.log(typeof statement)
+
+//Number 6
+let book
+let school
+let Number
+let group
+
+
+// Number 7
+book = "The Journey"
+school = "Pentvars"
+number = 43
+group = "TeamWin"
+
+
+//Number 8
+let Firstname = "Sylevster"
+let Lastname = "Baffour"
+let MaritalStatus = "Single"
+let country = "Ghana"
+ age = 22
+
+
+//Number 9
+let Fname = "Sylvester", Lname = "Baffour", MariStatus = "single", countrI = "Ghana", ageS ="22"
+
+
+
+// number 10
+
+let myAge = 34
+let yourAge =54
+
+console.log("I am",myAge,"old")
+console.log("I am",yourAge,"old")
+
---
+++ Day1/introduction.js
@@ -0,0 +1,6 @@
+let firstname = "Sylvester"
+let lastname = "Baffour"
+let age = 22
+console.log(firstname)
+console.log(lastname)
+console.log(age)
